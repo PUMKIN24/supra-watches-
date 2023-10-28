2011f0ca30fa4fb1d358bdd60f8b35f226116f70
PUMKIN24 <jer2ymatheww@gmail.com>
2023-10-28 12:05:31 +0530

    user profile

--- controllers/authController.js
+++ controllers/authController.js
@@ -149,3 +149,38 @@ export const forgotPassword = async (req, res) => {
         })
     }
 }
+
+//update profile
+export const updateProfileController = async (req, res) => {
+    try {
+        const { name, email, password, address, phone } = req.body;
+        const user = await userModel.findById(req.user._id);
+        //password
+        if (password && password.length < 5) {
+            return res.json({ error: "Password should contain minimum of five characters" });
+        }
+        const hashedPassword = password ? await hashPassword(password) : undefined;
+        const updatedUser = await userModel.findByIdAndUpdate(
+            req.user._id,
+            {
+                name: name || user.name,
+                password: hashedPassword || user.password,
+                phone: phone || user.phone,
+                address: address || user.address,
+            },
+            { new: true }
+        );
+        res.status(200).send({
+            success: true,
+            message: "Profile Updated SUccessfully",
+            updatedUser,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send({
+            success: false,
+            message: "Error WHile Update profile",
+            error,
+        });
+    }
+};
