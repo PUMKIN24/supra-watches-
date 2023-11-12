65a7bab9a1a47ee0376665477b7096bf9023369b
PUMKIN24 <jer2ymatheww@gmail.com>
2023-11-12 22:42:16 +0530

    more fixes

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -8,21 +8,21 @@ import { Badge } from "antd";
 import useCategory from './../../Hooks/useCategory';
 
 const Header = () => {
-    const [auth, setAuth] = useAuth();
+    const [auth, setAuth] = useAuth()
     const [cart] = useCart();
     const categories = useCategory();
     const handleLogout = () => {
         setAuth({
             ...auth,
             user: null,
-            token: "",
-        });
-        localStorage.removeItem("auth");
-        toast.success("Logout Successfully");
-    };
+            token: ''
+        })
+        localStorage.removeItem('auth')
+        toast.success("Logout Success")
+    }
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <button
                         className="navbar-toggler"
@@ -35,63 +35,32 @@ const Header = () => {
                     >
                         <span className="navbar-toggler-icon" />
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link to="/" className="navbar-brand">
-                            🛒 Ecommerce App
-                        </Link>
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <SearchInput />
-                            <li className="nav-item">
-                                <NavLink to="/" className="nav-link ">
-                                    Home
-                                </NavLink>
-                            </li>
-                            <li className="nav-item dropdown">
-                                <Link
-                                    className="nav-link dropdown-toggle"
-                                    to={"/categories"}
-                                    data-bs-toggle="dropdown"
-                                >
-                                    Categories
-                                </Link>
-                                <ul className="dropdown-menu">
-                                    <li>
-                                        <Link className="dropdown-item" to={"/categories"}>
-                                            All Categories
-                                        </Link>
-                                    </li>
-                                    {categories?.map((c) => (
-                                        <li>
-                                            <Link
-                                                className="dropdown-item"
-                                                to={`/category/${c.slug}`}
-                                            >
-                                                {c.name}
-                                            </Link>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </li>
+                    <Link to="/" className=" nav px-5" >supra watches</Link >
 
-                            {!auth?.user ? (
+                    <div className="collapse navbar-collapse m-2" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto">
+                            <SearchInput />
+                            <li className="nav-item ">
+                                <NavLink to="/" className="nav-link"> Home <span className="sr-only"></span></NavLink >
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to='/categories'
+                                    className="nav-link">Category <span className="sr-only"></span></NavLink >
+                            </li>
+                            {!auth.user ? (
                                 <>
                                     <li className="nav-item">
-                                        <NavLink to="/register" className="nav-link">
-                                            Register
-                                        </NavLink>
+                                        <NavLink to="/register" className="nav-link" >Register  </NavLink >
                                     </li>
                                     <li className="nav-item">
-                                        <NavLink to="/login" className="nav-link">
-                                            Login
-                                        </NavLink>
+                                        <NavLink to="/login" className="nav-link" >Login  </NavLink >
                                     </li>
-                                </>
-                            ) : (
+                                </>) : (
+
                                 <>
                                     <li className="nav-item dropdown">
                                         <NavLink
                                             className="nav-link dropdown-toggle"
-                                            href="#"
                                             role="button"
                                             data-bs-toggle="dropdown"
                                             style={{ border: "none" }}
@@ -100,40 +69,38 @@ const Header = () => {
                                         </NavLink>
                                         <ul className="dropdown-menu">
                                             <li>
-                                                <NavLink
-                                                    to={`/dashboard/${auth?.user?.role === 1 ? "admin" : "user"
-                                                        }`}
-                                                    className="dropdown-item"
-                                                >
-                                                    Dashboard
-                                                </NavLink>
+                                                <NavLink to={
+                                                    `/dashboard/${auth?.user?.role === 1 ? 'admin' : 'user'}`
+                                                } className="dropdown-item" >Dashboard</NavLink>
                                             </li>
                                             <li>
                                                 <NavLink
                                                     onClick={handleLogout}
                                                     to="/login"
-                                                    className="dropdown-item"
-                                                >
+                                                    className="dropdown-item">
                                                     Logout
                                                 </NavLink>
                                             </li>
                                         </ul>
                                     </li>
                                 </>
-                            )}
+                            )
+                            }
                             <li className="nav-item">
-                                <NavLink to="/cart" className="nav-link">
-                                    <Badge count={cart?.length} showZero offset={[10, -5]}>
+                                <Badge count={cart?.length} showZero>
+                                    <NavLink to="/cart" className="nav-link">
                                         Cart
-                                    </Badge>
-                                </NavLink>
+                                    </NavLink>
+                                </Badge>
                             </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
-    );
-};
 
-export default Header;
+                        </ul >
+
+                    </div >
+                </div >
+            </nav >
+        </>
+    )
+}
+
+export default Header
