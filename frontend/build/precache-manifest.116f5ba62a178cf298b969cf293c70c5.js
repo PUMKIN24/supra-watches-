a5c3d9dc62bdeb449e8783b8ddefe81252bcbc3c
PUMKIN24 <jer2ymatheww@gmail.com>
2023-11-12 22:08:54 +0530

    build fixed

---
+++ frontend/build/precache-manifest.116f5ba62a178cf298b969cf293c70c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8499084e0d447abc72fffabe25254cb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "548a517588b4debc0687",
+    "url": "/static/css/2.2a42abf6.chunk.css"
+  },
+  {
+    "revision": "341ef962680a1f0cb914",
+    "url": "/static/css/main.2a661885.chunk.css"
+  },
+  {
+    "revision": "548a517588b4debc0687",
+    "url": "/static/js/2.0b8337f5.chunk.js"
+  },
+  {
+    "revision": "b2b7c7cf83e75f81c625",
+    "url": "/static/js/3.fea09c08.chunk.js"
+  },
+  {
+    "revision": "341ef962680a1f0cb914",
+    "url": "/static/js/main.0fd28318.chunk.js"
+  },
+  {
+    "revision": "75015fa4bc231d33efc0",
+    "url": "/static/js/runtime~main.708bf333.js"
+  }
+]);
